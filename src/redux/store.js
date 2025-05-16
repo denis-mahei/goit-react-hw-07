5787c3c23d34c3ffa373e3471b76b3d80f14e878
denis-mahei <denmahei@gmail.com>
2025-05-16 09:56:14 +0300

    delete persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
 import contactsReducer from './contactsSlice.js';
 import filtersReducer from './filtersSlice.js';
-import storage from 'redux-persist/lib/storage';
-
-const persistConfig = {
-  key: 'contacts',
-  storage,
-};
-
-const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: persistedContactsReducer,
+    contacts: contactsReducer,
     filters: filtersReducer,
   },
 });
-
-export const persistor = persistStore(store);
