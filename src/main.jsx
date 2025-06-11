485f9226fd3d8b29ac8b7be14e6d37880300ba07
denis-mahei <denmahei@gmail.com>
2025-06-11 19:13:05 +0300

    bd change

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
-import 'modern-normalize';
 import App from './components/App.jsx';
+import 'modern-normalize';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
